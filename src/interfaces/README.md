3f4c7803596a4227299ea7dbbcf967b27c34e792
Michael Beutler <michael.beutler@iperka.com>
2021-09-11 11:58:37 +0200

    Moved interfaces submodule.

---
+++ src/interfaces/README.md
@@ -0,0 +1,3 @@
+# interfaces
+
+Interfaces for QueR
---
+++ src/interfaces/models.ts
@@ -0,0 +1,50 @@
+export interface User {
+    uuid: string;
+    firstName: string;
+    lastName: string;
+    birthdate: Date;
+    email: string;
+    phoneNumber?: string;
+    festivals: FestivalUser[];
+}
+
+export interface FestivalUser {
+    festival: Festival;
+    balance: number;
+    payments: Payment[];
+    role: Role;
+    imageUrl?: string;
+}
+
+export interface Festival {
+    uuid: string;
+    name: string;
+    description?: string;
+    location?: string;
+}
+
+export type Role = "visitor" | "employee" | "organisator";
+
+export interface Payment {
+    uuid: string;
+    amount: number;
+    description?: string;
+    festival: Festival;
+}
+
+export interface QRCode {
+    uuid: string;
+    user: User;
+    festival: Festival;
+}
+
+export interface Response<T> {
+    status: number;
+    message: string;
+    data: T;
+    errors?: {
+        code: number;
+        message: string;
+        debug?: string;
+    }[];
+}

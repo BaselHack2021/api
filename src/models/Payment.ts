4b246d5a991d4b2e294a0c4d9d2c2c47b5d270f8
Lukas <37246258+1H0@users.noreply.github.com>
2021-09-12 13:32:14 +0200

    Fix Schema

--- src/models/Payment.ts
+++ src/models/Payment.ts
@@ -5,9 +5,7 @@ import { getFestivalUserById, updateFestivalUserById } from './FestivalUser';
 const schema = new Schema<Payment>({
   amount: { type: Number, required: true },
   description: String,
-  festival: { type: Schema.Types.ObjectId, ref: 'Festival' },
   festivalUser: { type: Schema.Types.ObjectId, ref: 'Festivaluser', required: true },
-  status: { type: String, enum: ['insufficientBalance', 'insufficientAge', 'accepted'], required: true },
 });
 
 const PaymentModel = model<Payment>('Payment', schema);

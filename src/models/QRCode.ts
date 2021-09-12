3f48eeba6b771f7d5e72f83c0f14193729cb249d
Lukas <37246258+1H0@users.noreply.github.com>
2021-09-12 10:08:26 +0200

    Fix #2 to please @michaelbeutler

--- src/models/QRCode.ts
+++ src/models/QRCode.ts
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 import { QRCode } from '@baselhack2021/interfaces/models';
 
 const schema = new Schema<QRCode>({
-  user: { type: Schema.Types.ObjectId, ref: 'FestivalUser', required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'FestivalUser' },
 });
 
 const QRCodeModel = model<QRCode>('QRCode', schema);
